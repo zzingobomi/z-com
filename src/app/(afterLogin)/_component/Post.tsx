458e845f0baa15d815306658671c613d0fac89b9
Oscar_오종찬 <jcoh@maxst.com>
2024-12-30 16:35:42 +0900

    post

---
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import style from "./post.module.scss";
+import ActionButtons from "./ActionButtons";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+export default function Post() {
+  const target = {
+    User: {
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/yRsRRjGO.jpg",
+    },
+    content: "귀여운 승연이",
+    createdAt: new Date(),
+    Images: [],
+  };
+
+  return (
+    <article className={style.post}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            <div className={style.postShade} />
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={style.postImageSection}></div>
+          <ActionButtons />
+        </div>
+      </div>
+    </article>
+  );
+}
